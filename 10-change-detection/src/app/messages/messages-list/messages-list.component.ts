e53a7e3f4031252fbaa8fc094dec86484f0fa7df
KJChucci <kevinchucci@gmail.com>
2026-07-21 10:03:25 -0400

    section complete

--- 10-change-detection/src/app/messages/messages-list/messages-list.component.ts
+++ 10-change-detection/src/app/messages/messages-list/messages-list.component.ts
@@ -1,13 +1,38 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { MessagesService } from '../messages.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MessagesListComponent {
-  messages = input.required<string[]>();
+  private messageService = inject(MessagesService);
+  messages = this.messageService.allMessages;
+
+  // messages$ = this.messageService.messages$;
+
+  /*
+
+  private cdRef = inject(ChangeDetectorRef);
+  private destroyRef = inject(DestroyRef);
+
+  messages: string[] = [];
+
+  ngOnInit(): void {
+    const subscription = this.messageService.messages$.subscribe((messages) => {
+      this.messages = messages;
+      this.cdRef.markForCheck();
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
+
+  */
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
